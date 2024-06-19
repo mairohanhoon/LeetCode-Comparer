a736de6b4fc172ec18af2c490312350b63794c43
mairohanhoon <rohantiwari2709@gmail.com>
2024-06-19 22:48:20 +0530

    New Project create with base react files

---
+++ LeetCode_Comparer/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-white bg-slate-700 p-4 text-center m-2 text-3xl'>Rohan Tiwari</h1>
+    </>
+  )
+}
+
+export default App
