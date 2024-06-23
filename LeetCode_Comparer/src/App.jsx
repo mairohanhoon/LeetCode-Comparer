ce0d87176f8773d5d2ed35a17d9ce8d8c0ca0cd0
mairohanhoon <rohantiwari2709@gmail.com>
2024-06-23 12:12:31 +0530

    Api call added

--- LeetCode_Comparer/src/App.jsx
+++ LeetCode_Comparer/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Github from './components/Github/Github.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-white bg-slate-700 p-4 text-center m-2 text-3xl'>Rohan Tiwari</h1>
+    <div className='flex flex-row justify-center items-between'>
+    <Github/>
+    <Github/>
+    </div>
+      
     </>
   )
 }
