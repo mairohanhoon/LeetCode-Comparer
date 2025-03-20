3c39c37773e25e48a47036ac750c79e04846a1d3
mairohanhoon <rohantiwari2709@gmail.com>
2025-03-20 14:27:49 +0530

    Add Card and Input components to enhance UI layout

--- LeetCode_Comparer/src/App.jsx
+++ LeetCode_Comparer/src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card.jsx'
 
 function App() {
 
   return (
-    <div>
-      <div>Card1</div>
-      <div>Card2</div>
+    <div className='flex items-center justify-evenly h-screen'>
+      
+      <div>
+        
+        <Card/>
+      </div><div>
+        <Card/>
+      </div>
+
     </div>
   )
 }
---
+++ LeetCode_Comparer/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Card = ()=>{
+    return(
+        <div>
+            <p>Rohan Tiwari</p>
+            <p>Easy: 2 / 100</p>
+            <p>Medium: 2 / 100</p>
+            <p>Hard: 1 / 100</p>
+        </div>
+    )
+}
+
+export default Card;
