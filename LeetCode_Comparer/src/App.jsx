dd2fb5521d815c8bfdf2cf582c38cf6c3ff7681b
mairohanhoon <rohantiwari2709@gmail.com>
2025-03-20 14:27:21 +0530

    Refactor project to integrate Tailwind CSS and update dependencies

--- LeetCode_Comparer/src/App.jsx
+++ LeetCode_Comparer/src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Github from './components/Github/Github.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    <div className='flex flex-row justify-center items-between'>
-    <Github/>
-    <Github/>
+    <div>
+      <div>Card1</div>
+      <div>Card2</div>
     </div>
-      
-    </>
   )
 }
 
